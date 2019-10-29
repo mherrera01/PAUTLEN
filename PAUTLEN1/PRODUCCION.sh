0d001e739b6c2f3d734f6bce1fae57c50e8d0052
pelayo717 <pelayorodriguez4@gmail.com>
2019-10-29 18:30:50 +0100

    sript cambiado

--- PAUTLEN1/PRODUCCION.sh
+++ PAUTLEN1/PRODUCCION.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
+if [ $# = 0  ]; then
+echo "ejemplo => "
+echo "sh PRODUCCION.sh CREAR_LIBRERIA generacion"
+echo "sh PRODUCCION.sh EJECUTABLE_MAIN main_s0_1"
+echo "sh PRODUCCION.sh EJECUTABLE_ASM  ejemploasm"
+fi
+
 if [ "$1" = "CREAR_LIBRERIA" ]; then
 echo "Creando libreria $2.o ..."
 gcc -c $2.c
 fi
 
-if [ "$1" = "CREAR_MAIN" ]; then
+if [ "$1" = "EJECUTABLE_MAIN" ]; then
 echo "Creando main $2 ..."
 gcc -c $2.c
 gcc -o $2 $2.c generacion.o
 rm $2.o
 fi
 
-if [ "$1" = "CREAR" ]; then
+if [ "$1" = "EJECUTABLE_ASM" ]; then
 echo "Creando objeto $2.o ..."
 nasm -g -o $2.o -f elf32 $2.asm
 echo "Enlazando para crear $2"
